c873201aafdc5ff797ed50cf072dd2f770dad089
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-05-07 23:11:13 -0400

    parsed all results into data; created PossesionPercent and ConversionRate calcs

--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -14,12 +14,7 @@ namespace SoccerStats
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
             var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
-            var fileContents = ReadFile(fileName);
-            string[] fileLines = fileContents.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-            foreach(var line in fileLines)
-            {
-                Console.WriteLine(line);
-            }
+            var fileContents = ReadSoccerResults(fileName);
             Console.ReadLine();
         }
 
@@ -29,6 +24,64 @@ namespace SoccerStats
             {
                 return reader.ReadToEnd();
             }
+        }
+
+        public static List<GameResult> ReadSoccerResults(string fileName)
+        {
+            var soccerResults = new List<GameResult>();
+            using (var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                reader.ReadLine();
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var gameResult = new GameResult();
+                    string[] values = line.Split(',');
+
+                    DateTime gameDate;
+                    if (DateTime.TryParse(values[0], out gameDate))
+                    {
+                        gameResult.GameDate = gameDate;
+                    }
+
+                    gameResult.TeamName = values[1];
+
+                    HomeOrAway homeOrAway;
+                    if (Enum.TryParse(values[2], out homeOrAway))
+                    {
+                        gameResult.HomeOrAway = homeOrAway;
+                    }
+
+                    int parseInt;
+                    if (int.TryParse(values[3], out parseInt))
+                    {
+                        gameResult.Goals = parseInt;
+                    }
+                    if (int.TryParse(values[4], out parseInt))
+                    {
+                        gameResult.GoalAttempts = parseInt;
+                    }
+                    if (int.TryParse(values[5], out parseInt))
+                    {
+                        gameResult.ShotsOnGoal = parseInt;
+                    }
+                    if (int.TryParse(values[6], out parseInt))
+                    {
+                        gameResult.ShotsOffGoal = parseInt;
+                    }
+
+                    double parseDouble;
+                    if (double.TryParse(values[7], out parseDouble))
+                    {
+                        gameResult.PossessionPercent = parseDouble;
+                    }
+
+                    soccerResults.Add(gameResult);
+                    
+                }
+            }
+            return soccerResults;
+     
         }
     }
 }
